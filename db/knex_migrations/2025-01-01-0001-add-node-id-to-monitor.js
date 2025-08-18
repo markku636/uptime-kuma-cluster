d502f856aaf7956e47a646a0e119fea58050f1ad
mark.ku <a4756830@gmail.com>
2025-08-18 10:35:26 +0800

    Revert "fix issue"
    This reverts commit 6adc52ff06d27d8f50968b19455984cb9fd0863d.

--- db/knex_migrations/2025-01-01-0001-add-node-id-to-monitor.js
+++ db/knex_migrations/2025-01-01-0001-add-node-id-to-monitor.js
@@ -1,15 +1,13 @@
 exports.up = function (knex) {
     return knex.schema.alterTable("monitor", function (table) {
         table.string("node_id", 50).nullable();
-    }).then(() => {
-        return knex.schema.raw("CREATE INDEX idx_node_id ON monitor (node_id)");
+        table.index("node_id", "idx_node_id");
     });
 };
 
 exports.down = function (knex) {
-    return knex.schema.raw("DROP INDEX idx_node_id ON monitor").then(() => {
-        return knex.schema.alterTable("monitor", function (table) {
-            table.dropColumn("node_id");
-        });
+    return knex.schema.alterTable("monitor", function (table) {
+        table.dropIndex("node_id", "idx_node_id");
+        table.dropColumn("node_id");
     });
 };
